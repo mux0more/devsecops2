0f683119893f6ec394c834007ced7258a9061609
mux0more <mxdvsclash@gmail.com>
2025-11-08 16:40:47 +0300

    default calculcator release

---
+++ calculator_v1.0.0.py
@@ -0,0 +1,8 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+action = input("Какое действие вы хотите сделать?: ")
+result = 0
+if action == "+":
+    result = a + b
+print(result)
+    
